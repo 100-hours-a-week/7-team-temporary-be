9c7b8018e352da1a4508918c8ca58c593cdb36d2
sam <simonana@naver.com>
2026-01-20 12:57:11 +0900

    feat: Users entity 생성

--- src/main/java/molip/server/common/entity/BaseEntity.java
+++ src/main/java/molip/server/common/entity/BaseEntity.java
@@ -3,24 +3,20 @@ package molip.server.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    protected LocalDateTime createdAt;
+  @CreatedDate
+  @Column(updatable = false)
+  protected LocalDateTime createdAt;
 
-    @LastModifiedDate
-    @Column(nullable = false)
-    protected LocalDateTime updatedAt;
+  @LastModifiedDate protected LocalDateTime updatedAt;
 
-    @Column
-    protected LocalDateTime deletedAt;
+  protected LocalDateTime deletedAt;
 }
---
+++ src/main/java/molip/server/user/entity/Users.java
@@ -0,0 +1,36 @@
+package molip.server.user.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import molip.server.common.entity.BaseEntity;
+import molip.server.common.enums.FocusTimeZone;
+import molip.server.common.enums.Gender;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Users extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String email;
+
+  private String password;
+
+  private String nickname;
+
+  @Enumerated(EnumType.STRING)
+  private Gender gender;
+
+  private LocalDate birth;
+
+  @Enumerated(EnumType.STRING)
+  private FocusTimeZone focusTimeZone;
+
+  private LocalDateTime dayEndTime;
+}
