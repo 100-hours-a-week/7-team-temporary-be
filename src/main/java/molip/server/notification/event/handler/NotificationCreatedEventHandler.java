bae012fb708a1919d936d55574896790d1449f4a
sam <simonana@naver.com>
2026-01-30 15:29:42 +0900

    feat: eventListener 테스트 위해 즉시 발행으로 변경

--- src/main/java/molip/server/notification/event/handler/NotificationCreatedEventHandler.java
+++ src/main/java/molip/server/notification/event/handler/NotificationCreatedEventHandler.java
@@ -3,8 +3,8 @@ package molip.server.notification.event.handler;
 import lombok.RequiredArgsConstructor;
 import molip.server.notification.event.NotificationCreatedEvent;
 import molip.server.notification.facade.NotificationCommandFacade;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -12,7 +12,7 @@ public class NotificationCreatedEventHandler {
 
     private final NotificationCommandFacade notificationCommandFacade;
 
-    @TransactionalEventListener
+    @EventListener
     public void handleNotificationCreated(NotificationCreatedEvent event) {
 
         notificationCommandFacade.createScheduleNotifications(event);
