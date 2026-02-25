dc0ba46c8028e7e3d6e30906bd28edfdc4d73b1b
sam <simonana@naver.com>
2026-02-25 14:30:34 +0900

    feat: 친구 요청 생성 비즈니스 로직 생성

---
+++ src/main/java/molip/server/friend/facade/FriendRequestCommandFacade.java
@@ -0,0 +1,24 @@
+package molip.server.friend.facade;
+
+import lombok.RequiredArgsConstructor;
+import molip.server.friend.dto.response.FriendRequestResponse;
+import molip.server.friend.service.FriendRequestService;
+import molip.server.user.entity.Users;
+import molip.server.user.service.UserService;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class FriendRequestCommandFacade {
+
+    private final FriendRequestService friendRequestService;
+    private final UserService userService;
+
+    @Transactional
+    public FriendRequestResponse sendFriendRequest(Long fromUserId, Long targetUserId) {
+        Users toUser = userService.getUser(targetUserId);
+
+        return friendRequestService.sendFriendRequest(fromUserId, toUser);
+    }
+}
