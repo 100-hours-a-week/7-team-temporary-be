19eeef6f7523f891aacc35c5ee0c5db0a7025d0b
sam <simonana@naver.com>
2026-02-25 20:02:40 +0900

    feat: 친구 요청 생성, 친구 생성 알림 설정

--- src/main/java/molip/server/common/enums/NotificationTitle.java
+++ src/main/java/molip/server/common/enums/NotificationTitle.java
@@ -2,7 +2,9 @@ package molip.server.common.enums;
 
 public enum NotificationTitle {
     SCHEDULE_CREATED("일정이 생성되었습니다."),
-    SCHEDULE_REMINDER("5분 후에 일정이 시작됩니다.");
+    SCHEDULE_REMINDER("5분 후에 일정이 시작됩니다."),
+    FRIEND_REQUESTED("친구 요청이 도착했습니다."),
+    FRIEND_CREATED("친구 관계가 생성되었습니다.");
 
     private final String value;
 
