9e19f3bce96bc6f1423a41b9d5d224d46e6515a9
sam <simonana@naver.com>
2026-03-03 07:56:34 +0900

    feat: testCode redis 의존성 삭제

--- src/main/java/molip/server/chat/config/ChatRedisPubSubConfig.java
+++ src/main/java/molip/server/chat/config/ChatRedisPubSubConfig.java
@@ -3,6 +3,7 @@ package molip.server.chat.config;
 import lombok.RequiredArgsConstructor;
 import molip.server.chat.realtime.ChatRoomRealtimePublisher;
 import molip.server.chat.realtime.ChatRoomRealtimeSubscriber;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
 @RequiredArgsConstructor
+@ConditionalOnProperty(prefix = "spring.data.redis", name = "enabled", havingValue = "true")
 public class ChatRedisPubSubConfig {
 
     private final ChatRoomRealtimeSubscriber chatRoomRealtimeSubscriber;
